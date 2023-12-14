10f36caf09d1bff1331e20899d15fe2c480a4073
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-14 15:14:53 +0100

    add User To Html

--- public/front.js
+++ public/front.js
@@ -1,12 +1,35 @@
 "use strict";
 console.log("front.js file was loaded");
 
-const url = "http://localhost:3000/users";
+const url = "http://localhost:3000/api/users";
 
 // gauti userius su fetch
-fetch(url)
-  .then((resp) => resp.json)
-  .then((data) => console.log("data ===", data))
-  .catch((error) => {
-    console.warn("ivyko klaida:", error);
-  });
+function getAll() {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => console.log("data ===", data))
+    .catch((error) => {
+      console.warn("ivyko klaida:", error);
+    });
+}
+getAll();
+
+// sukurti funkcija kuri priims pirma useri is back end ir atvaizduos ji html h3 tage http://localhost:3000/users/1
+
+const url1 = "http://localhost:3000/api/users/1";
+
+function getOneUser() {
+  fetch(url1)
+    .then((resp) => resp.json())
+    .then((data) => toHtml(data))
+    .catch((error) => {
+      console.warn("ivyko klaida:", error);
+    });
+}
+
+function toHtml(userOne) {
+  const h3El = document.createElement("h3");
+  h3El.textContent = `${userOne.name} is ${userOne.age} years old`;
+  document.body.append(h3El);
+}
+getOneUser();
--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const app = express(); // sukuriame musu aplikacija. GALIME SAKYTI KAD CIA SUKURIAME SERVERIO APLIKACIJA
+const cors = require("cors");
 const PORT = 3000;
+
+// Middleware
+app.use(cors()); //iskvieciame cors kad leistu musu backa nuskaityti visiems
 
 const users = [
   { name: "John", age: 25 },
   { name: "Jane", age: 30 },
   { name: "Bob", age: 35 },
 ];
+
 // route GET - /about.html
 app.get("/about.html", (req, res) => {
   res.send("Hello World!");
 });
+
 // route GET - / slesiukas yra lygus localhost:3000
 app.get("/", (req, res) => {
   res.send("Welcome to Homepage!");
 });
+
 // route GET - /users
-app.get("/users", (req, res) => {
+app.get("/api/users", (req, res) => {
   console.log("req.query ===", req.query);
   res.json(users);
 });
+
 // route GET - /users/1
-app.get("/users/1", (req, res) => {
+app.get("/api/users/1", (req, res) => {
   console.log("req.query ===", req.query);
   res.json(users[0]);
 });
